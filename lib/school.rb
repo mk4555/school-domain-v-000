a4d29d03e0c170e27b7748e0dd7e1ce04cde0389
Min Kim <mk4555@nyu.edu>
2017-10-31 22:04:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,11 @@ class School
   end
 
   def add_student(student, grade)
-
+    if !@roster.include?(grade)
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
   end
 end
